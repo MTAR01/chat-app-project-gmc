70c0ae416cac4cee828a69b3f852fe73cabf45b3
Aziz Sehli <sahliaziz629@gmail.com>
2025-05-06 01:48:28 +0200

    Update server.js

--- server.js
+++ server.js
@@ -23,3 +23,12 @@ io.on('connection', (socket) => {
 server.listen(3000, () => {
     console.log('Server running on port 3000');
 });
+const io = require("socket.io")(server);
+
+io.on("connection", (socket) => {
+    console.log("User connected:", socket.id);
+
+    socket.on("chatMessage", (msg) => {
+        io.emit("chatMessage", msg); // Broadcast message to all clients
+    });
+});
