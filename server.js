4256fe3cb9ba69ef3e3e9fa59058d0039b160052
Aziz Sehli <sahliaziz629@gmail.com>
2025-05-05 22:48:57 +0000

    added backend

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(express.static('frontend')); // Serve frontend files
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+
+    socket.on('chatMessage', (msg) => {
+        io.emit('chatMessage', msg); // Broadcast message to all clients
+    });
+
+    socket.on('disconnect', () => {
+        console.log('A user disconnected');
+    });
+});
+
+server.listen(3000, () => {
+    console.log('Server running on port 3000');
+});
